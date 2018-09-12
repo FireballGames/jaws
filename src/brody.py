7b6c718e42b30f6b46643875200943e450187254
Dmitry Kutsenko <d2emonium@gmail.com>
2018-09-12 18:24:54 +0300

    Flip player

--- src/brody.py
+++ src/brody.py
@@ -4,6 +4,10 @@ import pygame
 from globals import BLOCK, RES_ROOT
 
 
+LEFT = 0
+RIGHT = 1
+
+
 class Brody(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -11,11 +15,14 @@ class Brody(pygame.sprite.Sprite):
         self.width = 2 * BLOCK
         self.height = 2 * BLOCK
 
+        self.direction = LEFT
+
         self.speed = 4
 
         # if self.image is None:
         #     self.image = pygame.image.load(os.path.join(RES_ROOT, "brody.png"))
-        self.image = pygame.image.load(os.path.join(RES_ROOT, "brody.png"))
+        self.base_image = pygame.image.load(os.path.join(RES_ROOT, "brody.png"))
+        self.image = self.base_image
 
         self.rect = pygame.Rect((15 * BLOCK, BLOCK, self.width, self.height))
         self.movement = (0, 0)
@@ -27,5 +34,12 @@ class Brody(pygame.sprite.Sprite):
             self.rect = old_rect
 
     def move(self, x, y, field):
+        if x > 0:
+            self.image = pygame.transform.flip(self.base_image, True, False)
+            self.direction = RIGHT
+        elif x < 0:
+            self.image = self.base_image
+            self.direction = LEFT
+
         self.movement = (x * self.speed, y * self.speed)
         self.update(field)
